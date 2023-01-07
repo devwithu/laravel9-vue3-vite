2b5e8ab2210ace0a7bdc7f738090cbecbe7df012
devwithu <devwithu@naver.com>
2023-01-07 22:29:43 +0900

    add Vuetify3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,16 @@
-import './bootstrap';
+//import './bootstrap';
 import {createApp} from 'vue'
-
 import App from './components/App.vue'
 
-createApp(App).mount("#app")
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).mount("#app")
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,28 @@
 <template>
-    How To Install Vue 3 in Laravel 9 with Vite - TechvBlogs
+    <v-card
+        class="mx-auto"
+        max-width="344"
+        variant="outlined"
+    >
+        <v-card-item>
+            <div>
+                <div class="text-overline mb-1">
+                    OVERLINE
+                </div>
+                <div class="text-h6 mb-1">
+                    Headline
+                </div>
+                <div class="text-caption">Greyhound divisely hello coldly fonwderfully</div>
+            </div>
+        </v-card-item>
+
+        <v-card-actions>
+            <v-btn variant="outlined">
+                Button
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
 </template>
 
 <script>
